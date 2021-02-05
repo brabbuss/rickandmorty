c6c0a36d3e8ab91acb0da192054db7d746899263
Scott Brabson <scottbrabson@gmail.com>
2021-02-05 11:59:03 -0700

    Add randomizing to api call

--- src/modules/utilities/apiCalls.js
+++ src/modules/utilities/apiCalls.js
@@ -1,7 +1,8 @@
 export const getCharacterData = async () => {
   // const response = await fetch("https://httpstat.us/500");   // test endpoint for 500 errors
+  const randomPage = (Math.floor(Math.random() * 34)+1)
   const response = await fetch(
-    "https://rickandmortyapi.com/api/character/?page=34"
+    `https://rickandmortyapi.com/api/character/?page=${randomPage}`
   );
   if (response.status >= 200 && response.status <= 299) {
     // No errors
